4eb771ed101c0827fcf61c927b962f68521af8ac
haixingfang <40502901+haixingfang@users.noreply.github.com>
2021-03-17 21:33:27 +0100

    add a compound input e.g. NaCl

---
+++ input_NaCl.m
@@ -0,0 +1,19 @@
+cell = [5.6402 5.6402 5.6402 90 90 90];
+sgno = 225;
+atomparam(1).name = 'Na';
+atomparam(1).atomno = 11;
+atomparam(1).pos = [0.00000 0.0000 0.0000];
+atomparam(1).adp =0.01;
+atomparam(1).occ = 0.5;
+atomparam(1).symmulti = 192;
+
+atomparam(2).name = 'Cl';
+atomparam(2).atomno = 17;
+atomparam(2).pos = [0.50000 0.5000 0.5000];
+atomparam(2).adp =0.01;
+atomparam(2).occ = 0.5;
+atomparam(2).symmulti = 192;
+
+space_group_IT_number = 225;
+
+
